b052dd7b1bb61dc9b418327508935ef5dc7532b7
Roman Coceres <romanagcoceres@gmail.com>
2026-09-14 21:10:43 -0300

    test: commit verde del escenario (módulo 1)

--- test/unit/_escenario_falla.test.ts
+++
@@ -1,6 +0,0 @@
-import test from "node:test";
-import assert from "node:assert";
-
-test("falla a propósito (escenario del módulo 1, se saca en el commit verde)", () => {
-  assert.equal(1, 2);
-});
